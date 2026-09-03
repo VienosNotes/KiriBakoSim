d30f73c1dd8cb76d673ee935b665b280d7a7d3e0
Daisuke Aoki <vienosnotes@gmail.com>
2026-09-04 01:31:59 +0900

    ミューオンを発射できるようにした

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import './style.css';
 import * as THREE from 'three';
-import {toRad} from './utils/utils';
+import { LineBasicMaterial, Vector3} from "three";
+import {toRad, rnd, rndAngle} from './utils/utils';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 
+
+
+const kbWidth = 2;
+const kbHeight = 0.5;
+const kbDepth = 2;
+
+const lines: THREE.Line[] = [];
+const drops = [];
+
+
+initControls();
 const canvas = document.querySelector('#c')!;
 
 const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -29,12 +41,8 @@ function update()
     renderer.render(scene, camera);
 }
 
-
 function buildKiribako() {
-    const boxWidth = 2;
-    const boxHeight = 0.5;
-    const boxDepth = 2;
-    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+    const geometry = new THREE.BoxGeometry(kbWidth, kbHeight, kbDepth);
     const edges = new THREE.EdgesGeometry(geometry);
     const material = new THREE.LineBasicMaterial();
     const frame = new THREE.LineSegments(edges, material);
@@ -49,4 +57,41 @@ function buildLight() {
     const light = new THREE.DirectionalLight(color, intensity);
     light.position.set(-1, 2, 4);
     return light;
+}
+
+function getRandomPointInKb(width : number, height : number, depth: number) {
+    return new Vector3(
+        rnd(-width/2, width/2),
+        rnd(-height/2, height/2),
+        rnd(-depth/2, depth/2),
+    );
+}
+
+function initControls()
+{
+    const rMuon = (document.querySelector('#rand-muon') as HTMLButtonElement)!;
+    rMuon.addEventListener('click', () => castRandomMuon());
+    const clearLinesButton = (document.querySelector('#clear-lines') as HTMLButtonElement)!;
+    clearLinesButton.addEventListener('click', () => clearLines());
+
+}
+
+function castRandomMuon() {
+    console.log("muon!");
+    const point = getRandomPointInKb(kbWidth, kbHeight, kbDepth);
+    const angle = rndAngle();
+
+    const p1 = point.clone().addScaledVector(angle, -10);
+    const p2 = point.clone().addScaledVector(angle, 10);
+    const geometry = new THREE.BufferGeometry().setFromPoints([p1,p2]);
+    const material = new LineBasicMaterial({color: "orange"});
+    const line = new THREE.Line(geometry, material);
+
+    lines.push(line);
+    scene.add(line);
+}
+
+function clearLines(): void {
+    lines.forEach(l => scene.remove(l));
+    lines.splice(0);
 }
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,27 @@
-﻿
+﻿import {Vector3} from "three";
+
 
 export function toRad(degree: number) : number {
     return (degree / 360) * (2 * Math.PI);
+}
+
+export function rnd(min: number, max: number)
+{
+    const width = max - min;
+    return (Math.random() * width) - (width / 2);
+}
+
+export function rndAngle() : Vector3 {
+    // 単位球のランダムな高さ
+    const z = rnd(-1, 1);
+    // その高さで切った断面の円の半径
+    const r = Math.sqrt(1-z*z);
+    // 断面円の角度
+    const longitude = rnd(0, Math.PI * 2);
+
+    return new Vector3(
+        r * Math.cos(longitude),
+        r * Math.sin(longitude),
+        z
+    );
 }
